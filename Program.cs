4096b984b2955517f492ab1f095d8f5bb370bd89
William <booster@iprimus.com.au>
2020-02-25 13:37:33 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someString = "Pear";
+
+            Fruit? someFruit = null;
+
+            try
+            {
+                someFruit = someString.ConvertToEnum2<Fruit>();
+            }
+            catch {}
+
+            Console.WriteLine("somefruit: " + someFruit);
+
+            Console.ReadLine();
+        }
+    }
+
+    public enum Fruit
+    {
+        Apple,
+        Pear,
+        Orange
+    }
+
+    static class Extensions
+    {
+        public static T ConvertToEnum2<T>(this string myString)
+        {
+            T f = (T)Enum.Parse(typeof(T), myString.ToString());
+
+            return f;
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Readme
+======
+
+Question 2 from test
+
+Dot Net Core 3.0 aplication, developed with VS 2019 Community.
